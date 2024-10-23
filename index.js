6163a0335db4e3be550c8361d9357eafb8708957
vivekchandran <vivekchandran663@gmail.com>
2024-10-23 19:46:33 +0530

    initial commit

--- index.js
+++ index.js
@@ -23,7 +23,8 @@ app.use(cors({
 
 
 
-app.use(express.json())
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
 const port = process.env.PORT || 3000;
 const mongoUri = process.env.MONGO_URI;
